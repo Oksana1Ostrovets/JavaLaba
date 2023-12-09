e6f80ec7c0c7e94fd3644fdf147749d1eb6a5bb8
Oksana1Ostrovets <153353784+Oksana1Ostrovets@users.noreply.github.com>
2023-12-09 11:38:32 +0200

    Add files via upload

---
+++ La3/untitled/src/main/java/la3/ModifiedBankService.java
@@ -0,0 +1,33 @@
+package la3;
+
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.LinkedList;
+import java.util.List;
+
+public class ModifiedBankService {
+
+    public static List<Bank> sortByName(List<Bank> banks) {
+        List<Bank> sortedBanks = new LinkedList<>(banks);
+        Collections.sort(sortedBanks, Comparator.comparing(Bank::getName));
+        return sortedBanks;
+    }
+
+    public static List<Bank> filterByLocation(List<Bank> banks, String location) {
+        List<Bank> filteredBanks = new LinkedList<>();
+        for (Bank bank : banks) {
+            if (bank.getLocation().equals(location)) {
+                filteredBanks.add(bank);
+            }
+        }
+        return filteredBanks;
+    }
+
+    public static List<Account> getAllAccounts(List<Bank> banks) {
+        List<Account> allAccounts = new LinkedList<>();
+        for (Bank bank : banks) {
+            allAccounts.addAll(bank.getAccounts());
+        }
+        return allAccounts;
+    }
+}
