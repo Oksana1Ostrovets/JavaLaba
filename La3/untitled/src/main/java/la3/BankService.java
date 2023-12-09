6232273bfa19e18d0588f6fc9d75f4d05fbab132
Oksana1Ostrovets <153353784+Oksana1Ostrovets@users.noreply.github.com>
2023-12-09 10:56:15 +0200

    Update BankService.java

--- La3/untitled/src/main/java/la3/BankService.java
+++ La3/untitled/src/main/java/la3/BankService.java
@@ -1,26 +1,33 @@
 package la3;
 
+import java.util.Collections;
 import java.util.Comparator;
+import java.util.LinkedList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class BankService {
 
     public static List<Bank> sortByName(List<Bank> banks) {
-        return banks.stream()
-                .sorted(Comparator.comparing(Bank::getName))
-                .collect(Collectors.toList());
+        List<Bank> sortedBanks = new LinkedList<>(banks);
+        Collections.sort(sortedBanks, Comparator.comparing(Bank::getName));
+        return sortedBanks;
     }
 
     public static List<Bank> filterByLocation(List<Bank> banks, String location) {
-        return banks.stream()
-                .filter(bank -> bank.getLocation().equals(location))
-                .collect(Collectors.toList());
+        List<Bank> filteredBanks = new LinkedList<>();
+        for (Bank bank : banks) {
+            if (bank.getLocation().equals(location)) {
+                filteredBanks.add(bank);
+            }
+        }
+        return filteredBanks;
     }
 
     public static List<Account> getAllAccounts(List<Bank> banks) {
-        return banks.stream()
-                .flatMap(bank -> bank.getAccounts().stream())
-                .collect(Collectors.toList());
+        List<Account> allAccounts = new LinkedList<>();
+        for (Bank bank : banks) {
+            allAccounts.addAll(bank.getAccounts());
+        }
+        return allAccounts;
     }
 }
