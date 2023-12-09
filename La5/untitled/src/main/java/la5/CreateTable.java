4b2ef26dd211f1beb16e89b3a8fbbce139b3867b
Oksana1Ostrovets <153353784+Oksana1Ostrovets@users.noreply.github.com>
2023-12-09 11:16:06 +0200

    Update CreateTable.java

--- La5/untitled/src/main/java/la5/CreateTable.java
+++ La5/untitled/src/main/java/la5/CreateTable.java
@@ -9,36 +9,46 @@ public class CreateTable {
         try (Connection connection = DatabaseConnection.getConnection();
              Statement statement = connection.createStatement()) {
 
-            String createBankTable = "CREATE TABLE Bank (" +
-                    "id INT PRIMARY KEY AUTO_INCREMENT," +
-                    "name VARCHAR(255) NOT NULL," +
-                    "location VARCHAR(255) NOT NULL" +
-                    ")";
-            statement.executeUpdate(createBankTable);
-
-            String createAccountTable = "CREATE TABLE Account (" +
-                    "id INT PRIMARY KEY AUTO_INCREMENT," +
-                    "accountNumber VARCHAR(255) NOT NULL," +
-                    "balance DOUBLE NOT NULL," +
-                    "bank_id INT," +
-                    "FOREIGN KEY (bank_id) REFERENCES Bank(id)" +
-                    ")";
-            statement.executeUpdate(createAccountTable);
-
-            String createTransactionTable = "CREATE TABLE Transaction (" +
-                    "id INT PRIMARY KEY AUTO_INCREMENT," +
-                    "transactionId VARCHAR(255) NOT NULL," +
-                    "description VARCHAR(255) NOT NULL," +
-                    "amount DOUBLE NOT NULL," +
-                    "bank_id INT," +
-                    "FOREIGN KEY (bank_id) REFERENCES Bank(id)" +
-                    ")";
-            statement.executeUpdate(createTransactionTable);
+            createBankTable(statement);
+            createAccountTable(statement);
+            createTransactionTable(statement);
 
             System.out.println("Tables created successfully.");
 
         } catch (SQLException e) {
             e.printStackTrace();
         }
+    }
+
+    private static void createBankTable(Statement statement) throws SQLException {
+        String createBankTable = "CREATE TABLE Bank (" +
+                "id INT PRIMARY KEY AUTO_INCREMENT," +
+                "name VARCHAR(255) NOT NULL," +
+                "location VARCHAR(255) NOT NULL" +
+                ")";
+        statement.executeUpdate(createBankTable);
+    }
+
+    private static void createAccountTable(Statement statement) throws SQLException {
+        String createAccountTable = "CREATE TABLE Account (" +
+                "id INT PRIMARY KEY AUTO_INCREMENT," +
+                "accountNumber VARCHAR(255) NOT NULL," +
+                "balance DOUBLE NOT NULL," +
+                "bank_id INT," +
+                "FOREIGN KEY (bank_id) REFERENCES Bank(id)" +
+                ")";
+        statement.executeUpdate(createAccountTable);
+    }
+
+    private static void createTransactionTable(Statement statement) throws SQLException {
+        String createTransactionTable = "CREATE TABLE Transaction (" +
+                "id INT PRIMARY KEY AUTO_INCREMENT," +
+                "transactionId VARCHAR(255) NOT NULL," +
+                "description VARCHAR(255) NOT NULL," +
+                "amount DOUBLE NOT NULL," +
+                "bank_id INT," +
+                "FOREIGN KEY (bank_id) REFERENCES Bank(id)" +
+                ")";
+        statement.executeUpdate(createTransactionTable);
     }
 }
