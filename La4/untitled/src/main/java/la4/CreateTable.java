9738a783e5db55529eef42748227674ec9ace537
Oksana1Ostrovets <153353784+Oksana1Ostrovets@users.noreply.github.com>
2023-12-09 10:20:45 +0200

    Add files via upload

---
+++ La4/untitled/src/main/java/la4/CreateTable.java
@@ -0,0 +1,44 @@
+package la4;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class CreateTable {
+    public static void main(String[] args) {
+        try (Connection connection = DatabaseConnection.getConnection();
+             Statement statement = connection.createStatement()) {
+
+            String createBankTable = "CREATE TABLE Bank (" +
+                    "id INT PRIMARY KEY AUTO_INCREMENT," +
+                    "name VARCHAR(255) NOT NULL," +
+                    "location VARCHAR(255) NOT NULL" +
+                    ")";
+            statement.executeUpdate(createBankTable);
+
+            String createAccountTable = "CREATE TABLE Account (" +
+                    "id INT PRIMARY KEY AUTO_INCREMENT," +
+                    "accountNumber VARCHAR(255) NOT NULL," +
+                    "balance DOUBLE NOT NULL," +
+                    "bank_id INT," +
+                    "FOREIGN KEY (bank_id) REFERENCES Bank(id)" +
+                    ")";
+            statement.executeUpdate(createAccountTable);
+
+            String createTransactionTable = "CREATE TABLE Transaction (" +
+                    "id INT PRIMARY KEY AUTO_INCREMENT," +
+                    "transactionId VARCHAR(255) NOT NULL," +
+                    "description VARCHAR(255) NOT NULL," +
+                    "amount DOUBLE NOT NULL," +
+                    "bank_id INT," +
+                    "FOREIGN KEY (bank_id) REFERENCES Bank(id)" +
+                    ")";
+            statement.executeUpdate(createTransactionTable);
+
+            System.out.println("Tables created successfully.");
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ La4/untitled/src/main/java/la4/Transaction.java
@@ -0,0 +1,100 @@
+package la4;
+
+import java.util.Objects;
+
+public class Transaction {
+    private String transactionId;
+    private String description;
+    private double amount;
+    private Bank bank;
+
+    public Transaction(String transactionId, String description, double amount) {
+        this.transactionId = transactionId;
+        this.description = description;
+        this.amount = amount;
+    }
+
+    public void setBank(Bank bank) {
+        this.bank = bank;
+    }
+
+    public Bank getBank() {
+        return bank;
+    }
+
+    public String getTransactionId() {
+        return transactionId;
+    }
+    public String getDescription() {
+        return description;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    @Override
+    public String toString() {
+        return "Transaction{" +
+                "transactionId='" + transactionId + '\'' +
+                ", description='" + description + '\'' +
+                ", amount=" + amount +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Transaction that = (Transaction) o;
+        return Double.compare(that.amount, amount) == 0 &&
+                Objects.equals(transactionId, that.transactionId) &&
+                Objects.equals(description, that.description);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(transactionId, description, amount);
+    }
+    public static class Builder {
+        private static final int MIN_TRANSACTION_ID_LENGTH = 1;
+        private static final int MAX_TRANSACTION_ID_LENGTH = 50;
+        private static final double MIN_AMOUNT = 0;
+
+        private String transactionId;
+        private String description;
+        private double amount;
+
+        public Builder() {
+        }
+
+        public Builder setTransactionId(String transactionId) {
+            if (transactionId == null || transactionId.length() < MIN_TRANSACTION_ID_LENGTH || transactionId.length() > MAX_TRANSACTION_ID_LENGTH) {
+                throw new IllegalArgumentException("Invalid transaction ID. Length should be between " +
+                        MIN_TRANSACTION_ID_LENGTH + " and " + MAX_TRANSACTION_ID_LENGTH + " characters.");
+            }
+            this.transactionId = transactionId;
+            return this;
+        }
+
+        public Builder setDescription(String description) {
+            this.description = description;
+            return this;
+        }
+
+        public Builder setAmount(double amount) {
+            if (amount < MIN_AMOUNT) {
+                throw new IllegalArgumentException("Invalid amount. Should be greater than or equal to " + MIN_AMOUNT);
+            }
+            this.amount = amount;
+            return this;
+        }
+
+        public Transaction build() {
+            if (transactionId == null) {
+                throw new IllegalArgumentException("Transaction ID is required.");
+            }
+            return new Transaction(this.transactionId, this.description, this.amount);
+        }
+    }
+}
